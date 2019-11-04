0780c6ad2a308bf9a2a4de57070b99078523dbba
wang-hai-cheng <wang-hai-cheng@foxmail.com>
2019-11-04 21:31:48 +0800

    new init

--- src/main/java/com/CodeGenerator.java
+++ src/main/java/com/CodeGenerator.java
@@ -56,7 +56,7 @@ public class CodeGenerator {
 
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql://rm-bp12dp9g0yluqn15uzo.mysql.rds.aliyuncs.com:3306/github_blog?useUnicode=true&useSSL=false&characterEncoding=utf8");
+        dsc.setUrl("jdbc:mysql://rm-bp12dp9g0yluqn15uzo.mysql.rds.aliyuncs.com:3306/admin_test?useUnicode=true&useSSL=false&characterEncoding=utf8");
         // dsc.setSchemaName("public");
         dsc.setDriverName("com.mysql.jdbc.Driver");
         dsc.setUsername("root");
